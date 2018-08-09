a13d1d5ad3facc70e94ea397928f3032060afa9b
Chang Li <lic2870@qq.com>
2018-08-09 14:50:19 +0800

    Add files via upload

---
+++ SwitchShadowrocketVPN/switchShadowrocketVPN.js
@@ -0,0 +1,16 @@
+/* 
+ * https://github.com/lic2870/JSBox/SwitchShadowrocketVPN
+ *@version 1.0
+ *@author lic2870
+ *@date 2018.8.3
+ *@note
+ * 1. 切换 Shadowrocket 的 VPN 状态
+ */
+
+
+var vpnState = $network.proxy_settings.HTTPEnable
+if (vpnState == 1) {
+  $app.openURL("shadowrocket://disconnect")
+} else {
+  $app.openURL("shadowrocket://connect")
+}
